e878522bd64b067bbc5a1b732ee5ebd748ad6ebe
Adrian Nalwalker <analwalker@planetfg.local>
2017-12-09 14:14:36 -0800

    initial

---
+++ hangman.js
@@ -0,0 +1,23 @@
+var inquirer = require('inquirer');
+
+var word = '';
+
+var guess = '';
+process.argv[2]
+
+var blanksAndSuccesses = [];
+
+var wrong = [];
+
+var mistakes = 5;
+
+var isGameStarted = false;
+
+//A function to start the game
+function start() {
+
+}
+
+// A function to take in guesses and check them against the answers
+
+// A funciton to go to the next round
