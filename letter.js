c03d5fc1b6424288f0123bf60268d2612d9e3a9c
Adrian Nalwalker <analwalker@planetfg.local>
2017-12-10 17:13:31 -0800

    array of words splits into letters, then splits into an array of letter objects with properties

--- letter.js
+++ letter.js
@@ -1,15 +1,11 @@
-var word = require('./word.js');
-// var hangman = require('./hangman.js');
-
 // Letter: Used for each letter in the current word. Each letter object should either display an underlying character, or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. This should contain letter specific logic and data
-function Letter(lett, guessed) {
+var Letters = function(lett, guessed) {
     this.lett = lett;
-    this.guessed = false;
+    this.guessed = guessed;
     this.hidden = '_ ';
     this.revealed = this.lett + ' ';
 };
-Letter();
-console.log(wordInPlay);
+
 // //loops through the word and creates an object for each letter or space.  letters are set to "guessed = false" while spaces are set to guessed = true"
 // Letter.prototype.split() {
 //     this
@@ -19,4 +15,4 @@ console.log(wordInPlay);
 // //displays all guessed and unguessed letters as " _ _ d   _ _ p p _ _ _ _" for expample.
 // Letter.prototype.display() {};
 
-module.exports = Letter;
+module.exports = Letters;
--- word.js
+++ word.js
@@ -1,14 +1,31 @@
-// var hangman = require('./hangman.js');
+var Letter = require('./letter.js');
 //an array of all hangman words - 70's rock bands
 var wordList = ['black sabbath', 'deep purple', 'kiss', 'ted nugent', 'judas priest', 'rush', 'rainbow', 'van halen', 'scorpions', 'motorhead', 'the who', 'aerosmith', 'thin lizzy', 'bad company', 'the rolling stones', 'pink floyd', 'jethro tull', 'zz top', 'foreigner', 'heart', 'slade', 'cheap trick', 'santana'];
 // Word: Used to create an object representing the current word the user is attempting to guess. This should contain word specific logic and data.
 var Word = function(currentWord) {
-    // selects at random frmo the array of words
+    // selects at random from the array of words
     this.currentWord = (wordList[Math.floor(Math.random() * wordList.length)]);
-    console.log(this.currentWord);
-    this.splitWord = currentWord.parse('');
+    // (Split) the selected word into an [array of individual letters and spaces].
+    this.splitWord = this.currentWord.split('');
     console.log(this.splitWord);
+    //an array to put the object letters in after they are constructed with the Letter constructor.
+    this.arrayOfObjects = [];
+    //a method to loop through the splitWord array pass each value into the Letter constructor.
+    this.primeArray = function() {
+        this.splitWord.forEach(function(letter) {
+            if (letter === ' ') {
+                this.arrayOfObjects.push(new Letter(letter, true));
+            } else {
+                this.arrayOfObjects.push(new Letter(letter, false));
+            }
+        })
+        console.log(this.arrayOfObjects);
+    }
+
+    this.primeArray();
 };
+
+
 
 Word();
 module.exports = Word;
