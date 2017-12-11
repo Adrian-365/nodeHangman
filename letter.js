7a5fbd7084c2d821c49bc59d36969d2587ef276a
Adrian Nalwalker <analwalker@planetfg.local>
2017-12-10 19:06:43 -0800

    display isn't working.  At best it pulls two words and puts them in the same array in hangman.js, but not in words.js

--- letter.js
+++ letter.js
@@ -1,18 +1,9 @@
-// Letter: Used for each letter in the current word. Each letter object should either display an underlying character, or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. This should contain letter specific logic and data
-var Letters = function(lett, guessed) {
+// Letter: A constructur used for each letter in the current word. Each letter object should either display an underlying character, or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. This should contain letter specific logic and data
+var Letter = function(lett, guessed) {
     this.lett = lett;
     this.guessed = guessed;
     this.hidden = '_ ';
     this.revealed = this.lett + ' ';
 };
 
-// //loops through the word and creates an object for each letter or space.  letters are set to "guessed = false" while spaces are set to guessed = true"
-// Letter.prototype.split() {
-//     this
-
-// }
-
-// //displays all guessed and unguessed letters as " _ _ d   _ _ p p _ _ _ _" for expample.
-// Letter.prototype.display() {};
-
-module.exports = Letters;
+module.exports = Letter;
--- word.js
+++ word.js
@@ -1,17 +1,19 @@
 var Letter = require('./letter.js');
 //an array of all hangman words - 70's rock bands
 var wordList = ['black sabbath', 'deep purple', 'kiss', 'ted nugent', 'judas priest', 'rush', 'rainbow', 'van halen', 'scorpions', 'motorhead', 'the who', 'aerosmith', 'thin lizzy', 'bad company', 'the rolling stones', 'pink floyd', 'jethro tull', 'zz top', 'foreigner', 'heart', 'slade', 'cheap trick', 'santana'];
-// Word: Used to create an object representing the current word the user is attempting to guess. This should contain word specific logic and data.
-var Word = function(currentWord) {
+// Word: a constructor used to create an object representing the current word the user is attempting to guess. This should contain word specific logic and data.
+var arrayOfObjects = []
+var Word = function() {
     // selects at random from the array of words
     this.currentWord = (wordList[Math.floor(Math.random() * wordList.length)]);
     // (Split) the selected word into an [array of individual letters and spaces].
     this.splitWord = this.currentWord.split('');
-    console.log(this.splitWord);
+    // console.log(this.splitWord);
     //an array to put the object letters in after they are constructed with the Letter constructor.
-    this.arrayOfObjects = [];
+
     //a method to loop through the splitWord array pass each value into the Letter constructor.
     this.primeArray = function() {
+        this.arrayOfObjects = [];
         this.splitWord.forEach(function(letter) {
             if (letter === ' ') {
                 this.arrayOfObjects.push(new Letter(letter, true));
