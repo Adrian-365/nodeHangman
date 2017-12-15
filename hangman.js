9b18ac6f5dee164e3e4c0f3359cacbb067db0847
Adrian Nalwalker <analwalker@planetfg.local>
2017-12-15 08:09:59 -0800

    fixed the array issue

--- hangman.js
+++ hangman.js
@@ -1,46 +1,60 @@
 var inquirer = require('inquirer');
 var Word = require('./word.js')
-    // a blank array to hold the blanks and letters that will be displayed in the terminal.
-
+    //an array of all hangman words - 70's rock bands
+var wordList = ['black sabbath', 'deep purple', 'kiss', 'ted nugent', 'judas priest', 'rush', 'rainbow', 'van halen', 'scorpions', 'motorhead', 'the who', 'aerosmith', 'thin lizzy', 'bad company', 'the rolling stones', 'pink floyd', 'jethro tull', 'zz top', 'foreigner', 'heart', 'slade', 'cheap trick', 'santana'];
 // a variable of guesses remaining
 var remaining = 10;
 
 
-//A function to start the game
+//this is just a short-version of the below function for purposes of checking the arrayOfObjects
 function Hangman() {
-    var displayArray = [];
-    var currentWord = new Word();
-    console.log('Console logging arrayofobjects from the hangman.js below:');
-    console.log(currentWord.arrayOfObjects);
-
-    function displayWord() {
-        for (i = 0; i < currentWord.arrayOfObjects.length; i++) {
-            if (currentWord.arrayOfObjects[i].guessed === false) {
-                displayArray.push(currentWord.arrayOfObjects[i].hidden);
-            } else if (this.arrayOfObjects[i].guessed === true) {
-                displayArray.push(this.arrayOfObjects[i].revealed);
-            }
-        };
-        console.log("Console logging the displayArray from the hangman.js below:")
-        console.log(displayArray);
-
-    };
-    displayWord();
+    // selects at random from the array of words
+    let currentWord = (wordList[Math.floor(Math.random() * wordList.length)]);
+    var wordInPlay = new Word(currentWord);
+    console.log('The wordInPlay from hangman.js:');
+    console.log(wordInPlay);
+    console.log('The arrayofobjects from hangman.js:');
+    console.log(wordInPlay.primeArray);
 
 
-    inquirer.prompt([{
-            name: "guess",
-            message: "Guess a letter then hit Enter!"
-        }]).then(function(answer) {
-            console.log(answer.guess);
-            currentWord.arrayOfObjects.forEach(function(answer) {
-                if (answer.guess === lett) {
-                    guessed === true;
-                }
-            });
-        })
-        // .then
-};
+}
+
+//A function to start the game
+// function Hangman() {
+//     var displayArray = [];
+//     var wordInPlay = new Word();
+//     console.log('The arrayofobjects from hangman.js:');
+//     console.log(wordInPlay.arrayOfObjects);
+
+//     function displayWord() {
+//         for (i = 0; i < wordInPlay.arrayOfObjects.length; i++) {
+//             if (wordInPlay.arrayOfObjects[i].guessed === false) {
+//                 displayArray.push(wordInPlay.arrayOfObjects[i].hidden);
+//             } else if (this.arrayOfObjects[i].guessed === true) {
+//                 displayArray.push(this.arrayOfObjects[i].revealed);
+//             }
+//         };
+//         console.log("The displayArray from hangman.js:")
+//         console.log(displayArray);
+
+//     };
+//     displayWord();
+
+
+//     inquirer.prompt([{
+//             name: "guess",
+//             message: "Guess a letter then hit Enter!"
+//         }]).then(function(answer) {
+//             console.log(answer.guess);
+//             wordInPlay.arrayOfObjects.forEach(function(answer) {
+//                 if (answer.guess === lett) {
+//                     guessed === true;
+//                 }
+//             });
+//         })
+//
+// };
+// };
 
 // calling the game function
 Hangman();
--- word.js
+++ word.js
@@ -1,37 +1,26 @@
 var Letter = require('./letter.js');
 //an array of all hangman words - 70's rock bands
-var wordList = ['black sabbath', 'deep purple', 'kiss', 'ted nugent', 'judas priest', 'rush', 'rainbow', 'van halen', 'scorpions', 'motorhead', 'the who', 'aerosmith', 'thin lizzy', 'bad company', 'the rolling stones', 'pink floyd', 'jethro tull', 'zz top', 'foreigner', 'heart', 'slade', 'cheap trick', 'santana'];
+// var wordList = ['black sabbath', 'deep purple', 'kiss', 'ted nugent', 'judas priest', 'rush', 'rainbow', 'van halen', 'scorpions', 'motorhead', 'the who', 'aerosmith', 'thin lizzy', 'bad company', 'the rolling stones', 'pink floyd', 'jethro tull', 'zz top', 'foreigner', 'heart', 'slade', 'cheap trick', 'santana'];
 // Word: a constructor used to create an object representing the current word the user is attempting to guess. This should contain word specific logic and data.
-var Word = function() {
-    // selects at random from the array of words
-    this.currentWord = (wordList[Math.floor(Math.random() * wordList.length)]);
+var Word = function(currentWord) {
+    // // selects at random from the array of words
+    this.currentWord = currentWord;
     // (Split) the selected word into an [array of individual letters and spaces].
-    this.splitWord = this.currentWord.split('');
-    // console.log(this.splitWord);
-    //an array to put the object letters in after they are constructed with the Letter constructor.
-    this.arrayOfObjects = [];
+
+
     //a method to loop through the splitWord array pass each value into the Letter constructor.
-    this.primeArray = function() {
+    this.primeArray = this.currentWord.split('').map(function(letter) {
+        if (letter === ' ') {
+            return new Letter(letter, true);
+        } else {
+            return new Letter(letter, false);
+        }
+    })
 
-        this.splitWord.forEach(function(letter) {
-            if (letter === ' ') {
-                this.arrayOfObjects.push(new Letter(letter, true));
-            } else {
-                this.arrayOfObjects.push(new Letter(letter, false));
-            }
-        })
 
-    }
-
-    this.primeArray();
-    console.log("THE ARRAY OF LETTER OBJECTS from the Word constructor:")
+    console.log("THE 'arrayOfObjects' from word.js:")
     console.log(this.arrayOfObjects);
-
 };
 
-
-
-Word();
-
-
+// Word();
 module.exports = Word;
